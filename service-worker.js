8dd6896a7dd6a7d60aa070b8dedce2ad7b9356b8
Iefan <66840141+realiefan@users.noreply.github.com>
2023-04-17 02:17:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,21 @@
-const CACHE_NAME = "my-pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/logo.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => Promise.all(
-        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-          .map(cacheName => caches.delete(cacheName))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+      caches.open("zapit-cache").then((cache) => {
+        return cache.addAll([
+          "/",
+          "logo.png",
+          "paywall.css",
+          "app.js",
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
